3046004e60bf1af4e8b541be8acda7e109f8ce58
Dob <janghanan11@gmail.com>
2025-11-26 00:17:07 +0800

    Refactor LeaderboardFragment by removing unused components
    Removed TabLayout and ViewPager2 setup from LeaderboardFragment.

--- app/src/main/java/com/itismob/s15/group3/mco/project/LeaderboardFragment.java
+++ app/src/main/java/com/itismob/s15/group3/mco/project/LeaderboardFragment.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,18 +16,6 @@ public class LeaderboardFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_leaderboard, container, false);
-
-        TabLayout tabLayout = v.findViewById(R.id.tabLayout);
-        ViewPager2 viewPager = v.findViewById(R.id.viewPager);
-
-        LeaderboardPagerAdapter adapter = new LeaderboardPagerAdapter(this);
-        viewPager.setAdapter(adapter);
-
-        new TabLayoutMediator(tabLayout, viewPager,
-                (tab, position) -> tab.setText(position == 0 ? "Friends" : "Global")
-        ).attach();
-
-        return v;
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
     }
 }
